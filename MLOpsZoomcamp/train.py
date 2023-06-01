2ffb09f566ba909e4e5c32a66f91e35043bae86d
changhyeonlim <changhyeonlim@gmail.com>
2023-05-31 21:53:21 -0400

    enables autologging using mlflow

--- MLOpsZoomcamp/train.py
+++ MLOpsZoomcamp/train.py
@@ -5,7 +5,7 @@ import click
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-
+import mlflow
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
@@ -21,13 +21,21 @@ def run_train(data_path: str):
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.strart():
+        mlflow.set_tag("developer", "samlim")
+        mlflow.log_param("train_data_path", os.path.join(data_path, "train.pkl"))
+        mlflow.log_param("valid-data-path", os.path.join(data_path, "val.pkl"))
+        md = 10
+        mlflow.log_param("max_depth", md)
+        rf = RandomForestRegressor(max_depth=md, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("hw2-experiment")
     run_train()
