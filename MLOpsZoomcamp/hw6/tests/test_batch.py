defc2ea3a4e9a37196f0c2ca629b442a449d4e84
changhyeonlim <changhyeonlim@gmail.com>
2023-07-16 09:25:54 -0400

    initial commit

---
+++ MLOpsZoomcamp/hw6/tests/test_batch.py
@@ -0,0 +1,36 @@
+import batch
+from datetime import datetime
+import pandas as pd
+
+def dt(hour, minute, second=0):
+    return datetime(2022, 1, 1, hour, minute, second)
+
+def prepare_data(df, categorical = ['PULocationID', 'DOLocationID']):
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+def read_data(data):
+    columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    df = pd.DataFrame(data, columns=columns)
+    return df
+
+
+def testing():
+    data = [
+        (None, None, dt(1, 2), dt(1, 10)),
+        (1, None, dt(1, 2), dt(1, 10)),
+        (1, 2, dt(2, 2), dt(2, 3)),
+        (None, 1, dt(1, 2, 0), dt(1, 2, 50)),
+        (2, 3, dt(1, 2, 0), dt(1, 2, 59)),
+        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),     
+    ]
+    test_df = read_data(data)
+    test_df = prepare_data(test_df)
+    
+    assert len(test_df) == 3
